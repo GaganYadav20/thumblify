e55a8620d3081812ede3832a1a00d933600b1d49
Gagan Yadav <gaganyadav2094@gmail.com>
2026-02-08 09:00:48 +0530

    first commit

---
+++ src/data/features.tsx
@@ -0,0 +1,20 @@
+import type { IFeature } from "../types";
+
+export const featuresData: IFeature[] = [
+    {
+        icon:"/assets/zap-icon.svg",
+        title: "Smart analysis",
+        description: "Our AI analyzes video contentto suggest the most clickable concepts.",
+    },
+    {
+        icon:"/assets/thumb-icon.svg",
+        title: "Eye-catching Designs",
+        description: "Generate vibrant , high-contrast thumbnail that stand out in the feed.",
+    },
+    {
+        icon:"/assets/shape-icon.svg",
+        title: "Fully Editable",
+        description: "Get fully layered designs you can tweak to perfection if needed.",
+    },
+    
+];
