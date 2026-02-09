1941b17540edb476e561c5cf11efdebd004a85f1
Gagan Yadav <gaganyadav2094@gmail.com>
2026-02-09 16:40:43 +0530

    edit generate page

--- src/pages/Generate.tsx
+++ src/pages/Generate.tsx
@@ -1,8 +1,16 @@
+import { useState } from "react";
+import { useParams } from "react-router-dom"
 
 
 const Generate = () => {
+
+  const {id}=useParams();
+  const [title,setTitle]=useState('');
+  const [additionalDetails,setAdditionalDetails]=useState('');
+  // const [thumbnail,setThumbnail]=useState<IThumbnail>(null);
   return (
     <div>
+
       
     </div>
   )
